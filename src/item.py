ca8d13738021b2d84883fdd6a90899a086edcfab
Tanzhor_RIK <tanzhor4@gmail.com>
2023-04-27 00:15:16 +0700

    fixed metod name

--- src/item.py
+++ src/item.py
@@ -27,7 +27,7 @@ class Item:
 
     @name.setter
     def name(self, name):
-        if len(self.name) > 10:
+        if len(name) > 10:
             raise Exception("Длина наименования товара превышает 10 символов.")
         else:
             self.__name = name
